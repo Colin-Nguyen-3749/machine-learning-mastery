c7e5a4c41c72198f3b1271e1af3ce48742c74ecd
Colin Nguyen <nguyen_c9@denison.edu>
2026-07-09 15:49:53 -0400

    Confirm dimensions of dataset

--- machine.py
+++ machine.py
@@ -19,3 +19,9 @@ from sklearn.svm import SVC
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
+
+# Check dimensions of the dataset
+# print("Dimensions of dataset: " + str(dataset.shape))
+
+# Peek at the dataset
+print(dataset.head(20))
